0c2681faf58d50476a98fde492fe4f1309791129
Weiwei <93496699+wei858910@users.noreply.github.com>
2025-04-26 13:13:10 +0800

    Initial Project

---
+++ Source/FlappyBird/FlappyBird.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FlappyBird.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FlappyBird, "FlappyBird" );
